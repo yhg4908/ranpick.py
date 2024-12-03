d9059a7bf586c71fbb41177ae77f48cf5062b6f2
rainy58 <119713626+yhg4908@users.noreply.github.com>
2024-12-03 16:40:12 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ranpick',
+    version='0.1.0',
+    author='rainy58',
+    author_email='yhg4908@kakao.com',
+    description='A high-entropy random number generation library',
+    long_description=open('README.md').read() if open('README.md', 'r').read() else '',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yhg4908/ranpick.py',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires='>=3.7',
+    keywords='random number generation entropy ranpick',
+)
