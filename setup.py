e3fbbe5451b4862c25f023dccf836e55eb7eeaad
rainy5810 <yhg4908@naver.com>
2025-05-21 08:24:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     install_requires=[],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
